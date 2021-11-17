f6d7391de29690351ce38820dfa14838388dc53f
구민석 <ser_om@naver.com>
2021-11-17 20:58:13 +0900

    11/17

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -12,16 +12,19 @@ public class Ex08 {
 		int num2  = sc.nextInt();
 		System.out.print("숫자3: ");
 		int num3  = sc.nextInt();
-
-		if (num1<=num2 && num1<=num3) {
+		
+		int min = num1;
+		//------------------------------------
+		if (min<=num2 && min<=num3) {
 			System.out.println("가장 작은 수는 "+num1+" 입니다.");
 		}
-		else if (num2<=num1 && num2<=num3) {
+		else if (min<=num1 && num2<=num3) {
 			System.out.println("가장 작은 수는 "+num2+" 입니다.");
 		}
-		else if (num3<=num1 && num3<=num2) {
+		else if (min<=num1 && num3<=num2) {
 			System.out.println("가장 작은 수는 "+num3+" 입니다.");
 		}
+		//--------------------------------------
 		sc.close();
 	}
 
